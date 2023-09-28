ccaaaeecf7b908e35a1305dc21cebb7935697bc5
pelolsen <pelolsen@hotmail.com>
2023-09-28 17:47:45 +0200

    Working

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import CameraScreen from './components/CameraScreen';
+import ImageScreen from './components/ImageScreen';
 
 export default function App() {
+  const Stack = createStackNavigator();
+  /*Bruger stack navigator til at vise billeder taget fra app*/
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name={'home'} component={CameraScreen} options={{headerShown: false}} />
+        <Stack.Screen name={'image'} component={ImageScreen} />
+      </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
+/*
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,3 +27,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+*/
